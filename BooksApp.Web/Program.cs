380ed5e58582e91d1ea8bb4f2aa5a37a2ae9661f
10github100 <10github10@gmail.com>
2024-06-24 18:01:02 +0400

    fix

--- BooksApp.Web/Program.cs
+++ BooksApp.Web/Program.cs
@@ -75,33 +75,6 @@ builder.Services.AddSwaggerGen(option =>
 });
 
 
-//var settingsSection = builder.Configuration.GetSection("JwtOptions");
-
-//var secret = settingsSection.GetValue<string>("Secret");
-//var issuer = settingsSection.GetValue<string>("Issuer");
-//var audience = settingsSection.GetValue<string>("Audience");
-
-//var key = Encoding.ASCII.GetBytes(secret);
-
-//builder.Services.AddAuthentication(x =>
-//{
-//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//}).AddJwtBearer(x =>
-//{
-//    x.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuerSigningKey = true,
-//        IssuerSigningKey = new SymmetricSecurityKey(key),
-//        ValidateIssuer = true,
-//        ValidIssuer = issuer,
-//        ValidAudience = audience,
-//        ValidateAudience = true
-//    };
-//});
-//builder.Services.AddAuthorization();
-
-
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
